4083ea99eb4aacd9f29d21bd16b14434132d28b2
Abdelnacerr <abdelnacerr@gmail.com>
2021-02-04 10:21:36 +1100

    wordnik api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Facebook from "./components/Facebook";
+import Wordnik from "./wordnik";
 
 export default function App() {
   return (
     <div>
-      <Facebook />
+      <Wordnik />
     </div>
   );
 }
